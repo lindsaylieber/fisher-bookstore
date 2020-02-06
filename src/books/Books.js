c17134e4ca6670c0824feae2c63c6cf066bf3d52
Lindsay Lieber <lmlindsay510@gmail.com>
2020-02-05 20:22:02 -0500

    books page

---
+++ src/books/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Books.css";
+
+export default function Home() {
+    return (
+        <div className="Book">
+            <div className="lander">
+                <h1>Home</h1>
+                <p>Welcome to Fisher Bookstore.</p>
+            </div>
+        </div>
+    );
+}
